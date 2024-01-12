1b09114d7f45c6b18dd1785ffd82a6af8bb61b40
CodeYourFaceOff <codeyourfaceoff@gmail.com>
2024-01-12 13:49:46 -0600

    Setup docker + multitenancy v1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const orgName = process.env.ORG_NAME
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  basePath: `/${orgName.toLowerCase()}`,
+  assetPrefix: `/${orgName.toLowerCase()}`
+}
 
 module.exports = nextConfig
---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+const OrgName = process.env.ORG_NAME
+
+export default function AboutPage() {
+  return <div>About deez nutts we are org {OrgName}</div>
+}
